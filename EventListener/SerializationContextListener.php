29a719ec19944ade2e9a5aea7ce25e8e3e66c542
Vidy Videni <vidy.videni@gmail.com>
2019-03-25 11:41:52 +0800

    fix SerializationContextListener

--- EventListener/SerializationContextListener.php
+++ EventListener/SerializationContextListener.php
@@ -4,12 +4,20 @@ namespace Pintushi\Bundle\SecurityBundle\EventListener;
 
 use Videni\Bundle\RestBundle\Event\SerializationContextEvent;
 use Pintushi\Bundle\SecurityBundle\Serializer\Exclusion\AclProtectedFieldExclusionStrategy;
+use Pintushi\Bundle\SecurityBundle\Form\FieldAclHelper;
 
 class SerializationContextListener
 {
+    private $fieldAclHelper;
+
+    public function __construct(FieldAclHelper $fieldAclHelper)
+    {
+        $this->fieldAclHelper = $fieldAclHelper;
+    }
+
     public function addAclProtectedFieldExclusionStrategy(SerializationContextEvent $event)
     {
         $context = $event->getContext();
-        $context->addExclusionStrategy(new AclProtectedFieldExclusionStrategy());
+        $context->addExclusionStrategy(new AclProtectedFieldExclusionStrategy($this->fieldAclHelper));
     }
 }
